cb9cccd2d12cf1926a7f9bbab1070bd6b4519a1e
unknown <marko.kovacevic1112@gmail.com>
2021-12-13 13:27:52 +0100

    thirdTimeCommiting

---
+++ DZ3/WeatherPrognosis/WeatherPrognosis/UniformGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherPrognosis
+{
+    public class UniformGenerator : IRandomGenerator
+    {
+        private Random generator;
+
+        public UniformGenerator(Random generator)
+        {
+            this.generator = generator;
+        }
+
+        public double Generate(double min, double max)
+        {
+            return generator.NextDouble() * (max - min) + min;
+        }
+    }
+}
